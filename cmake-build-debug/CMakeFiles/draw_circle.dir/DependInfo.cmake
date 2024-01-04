36c98f82385782edc0ceb37645b2c5da27ba074f
George Hill <georgehill7924@gmail.com>
2024-01-04 16:37:11 +0000

    adding the .gitignore

---
+++ cmake-build-debug/CMakeFiles/draw_circle.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/george/CLionProjects/draw_circle/glad.c" "CMakeFiles/draw_circle.dir/glad.c.o" "gcc" "CMakeFiles/draw_circle.dir/glad.c.o.d"
+  "/home/george/CLionProjects/draw_circle/Circle.cpp" "CMakeFiles/draw_circle.dir/Circle.cpp.o" "gcc" "CMakeFiles/draw_circle.dir/Circle.cpp.o.d"
+  "/home/george/CLionProjects/draw_circle/Display.cpp" "CMakeFiles/draw_circle.dir/Display.cpp.o" "gcc" "CMakeFiles/draw_circle.dir/Display.cpp.o.d"
+  "/home/george/CLionProjects/draw_circle/Shader.cpp" "CMakeFiles/draw_circle.dir/Shader.cpp.o" "gcc" "CMakeFiles/draw_circle.dir/Shader.cpp.o.d"
+  "/home/george/CLionProjects/draw_circle/main.cpp" "CMakeFiles/draw_circle.dir/main.cpp.o" "gcc" "CMakeFiles/draw_circle.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/draw_circle.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/draw_circle.dir/Circle.cpp.o"
+  "CMakeFiles/draw_circle.dir/Circle.cpp.o.d"
+  "CMakeFiles/draw_circle.dir/Display.cpp.o"
+  "CMakeFiles/draw_circle.dir/Display.cpp.o.d"
+  "CMakeFiles/draw_circle.dir/Shader.cpp.o"
+  "CMakeFiles/draw_circle.dir/Shader.cpp.o.d"
+  "CMakeFiles/draw_circle.dir/glad.c.o"
+  "CMakeFiles/draw_circle.dir/glad.c.o.d"
+  "CMakeFiles/draw_circle.dir/main.cpp.o"
+  "CMakeFiles/draw_circle.dir/main.cpp.o.d"
+  "draw_circle"
+  "draw_circle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/draw_circle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
